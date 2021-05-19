02db5798421850a00e885f8a912711f5e692eb6d
Baptiste Martin <81614331+baptiste-martin-campus@users.noreply.github.com>
2021-05-19 16:31:49 +0200

    maj item

--- item.php
+++ item.php
@@ -1,9 +1,9 @@
 <?php
 
-$name = "Sombrero";
+$name = "Sombrero Mexicain";
 $price = 50;
 $picture = "https://www.cdiscount.com/pdt2/0/2/4/1/700x700/auc5026619070024/rw/sombrero-mexicain.jpg";
 
-echo "$name, $price,'€',$picture";
+echo "$name <br>$price € <br> <img src='$picture' alt='sombrero mexicain'/>";
 
 ?>
