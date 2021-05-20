330b417a4ee71001963ed864500765d45e95ee7c
Baptiste Martin <81614331+baptiste-martin-campus@users.noreply.github.com>
2021-05-20 11:56:44 +0200

    Create simple-catalog.php

---
+++ simple-catalog.php
@@ -0,0 +1,11 @@
+<?php
+
+$products = ["Low Green", "Mocha", "Game Royal"];
+$first_item = ($products[0]);
+$last_item = end($products);
+
+echo $first_item ;
+echo $last_item ;
+sort($products);
+print_r($products);
+?>
