90b6fefc8dcec87809bdba033828116f9663f7bb
Baptiste Martin <81614331+baptiste-martin-campus@users.noreply.github.com>
2021-05-20 10:32:25 +0200

    Initialisation site silent

---
+++ footer.php
@@ -0,0 +1,64 @@
+<!--DEBUT DE MON FOOTER-->
+<footer class="footer">
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-12 text-center" ><a href="#" id="footerfaq">FAQ</a> </div>
+
+      <div class="row">
+        <div class="col-12 text-center" ><a href="Formulaire_2.html" id="footercontact">CONTACT</a> </div>
+      </div>
+    </div>
+
+    <!-- Ligne de 12 colonnes -->
+    <div class="row row-cols-sm-1 row-cols-md-2 row-cols-lg-3" id="reseaux">
+      <!-- 1/3 de la largeur (largeur de 4 colonnes)-->
+      <div class="silent col">
+        <a href="index.html">
+          <img src="img/logo silent.jpg" alt="Logo silent"
+        /></a>
+        SILENT
+      </div>
+
+      <!-- 1/3 de la largeur (largeur de 4 colonnes)-->
+      <div class="col">
+        <div class="reseau">
+          <a href="https://www.facebook.com/RickAstley">
+            <img src="img/facebook_1.png" alt="Logo facebook"
+          /></a>
+
+          <a href="https://www.instagram.com/silent_fnf/">
+            <img src="img/insta_1.png" alt="Logo instagram"
+          /></a>
+
+          <a href="https://twitter.com/Silent_Fnf"
+            ><img src="img/twitter_1.png" alt="Logo twitter"
+          /></a>
+        </div>
+      </div>
+
+      <!-- 1/3 de la largeur (largeur de 4 colonnes)-->
+      <div class="col">
+        <div class="campus">
+          <a href="https://le-campus-numerique.fr/">
+            <img
+              src="img/logo_campus_header_1.png"
+              alt="Logo campus numerique"
+              class="logo-campus"
+          /></a>
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="texte">
+        <div class="col-12 text-center text-muted">
+          Site by Silent Corps <br />
+          Copyright © 2020, Silent<br />
+          Terms & Conditions - Privacy - Legal Notice
+        </div>
+      </div>
+    </div>
+  </div>
+</footer>
+<!--FIN DE MON FOOTER-->
+</body>
+</html>
