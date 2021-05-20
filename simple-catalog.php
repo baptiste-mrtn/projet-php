04218176abdf1071e609ce76a1ad1cb898ecacc7
Baptiste Martin <81614331+baptiste-martin-campus@users.noreply.github.com>
2021-05-20 11:58:56 +0200

    Update simple-catalog.php

--- simple-catalog.php
+++ simple-catalog.php
@@ -4,8 +4,9 @@ $products = ["Low Green", "Mocha", "Game Royal"];
 $first_item = ($products[0]);
 $last_item = end($products);
 
-echo $first_item ;
-echo $last_item ;
+echo $first_item;
+echo $last_item;
 sort($products);
 print_r($products);
+
 ?>
