c1db134ec535d6093e4bdf35c9cc5359b238e419
Baptiste Martin <81614331+baptiste-martin-campus@users.noreply.github.com>
2021-05-25 16:36:07 +0200

    maj multidimensional-catalog
    Fin exercice 4

--- multidimensional-catalog.php
+++ multidimensional-catalog.php
@@ -125,7 +125,7 @@ $products = [
 
     <div class="boucle-while">
       <h2>Boucle While</h2>
-      <p class="text-light">
+      <ul class='row col-4 list-group list-group-flush mx-auto mb-4 border bg-white text-center'>
       <?php
       $i = 0;
       $keys = array_keys($products);
@@ -136,10 +136,14 @@ $products = [
         $value = array_values($products[$keys[$i]]);
         while ($ii < count($keys_value)) {
           if ($ii === 4) {
+            echo "<li class='list-group-item'>";
             echo "$keys_value[$ii] : <img src='$value[$ii]' alt='image baskettes'><br>";
+            echo "</li>";
             $ii = $ii +1;
           } else {
-          echo "$keys_value[$ii] : $value[$ii]<br>";
+            echo "<li class='list-group-item'>";
+            echo "$keys_value[$ii] : $value[$ii]<br>";
+            echo "</li>";
           $ii = $ii +1;
         }
         }
@@ -147,27 +151,95 @@ $products = [
         $i = $i +1;
       }
       ?>
-    </p>
+    </ul>
   </div>
 
   <div class="boucle-do-while">
     <h2>Boucle Do-While</h2>
-
-    </p>
+        <ul class='row col-4 list-group list-group-flush mx-auto mb-4 border bg-white text-center'>
+        <?php
+        $i = 0;
+        $keys = array_keys($products);
+            do {
+                $ii = 0;
+                echo "$keys[$i] : <br>";
+                $keys_value = array_keys($products[$keys[$i]]);
+                $value = array_values($products[$keys[$i]]);
+                do {
+                    if ($ii === 4) {
+                      echo "<li class='list-group-item'>";
+                      echo "$keys_value[$ii] : <img src='$value[$ii]' alt='image baskettes'><br>";
+                      echo "</li>";
+                      $ii = $ii +1;
+                    } else {
+                      echo "<li class='list-group-item'>";
+                      echo "$keys_value[$ii] : $value[$ii]<br>";
+                      echo "</li>";
+                    $ii = $ii +1;
+                }
+                } while ($ii < count($keys_value));
+                echo "<br>";
+                $i = $i +1;
+            } while ($i < count($products));
+         ?>
+     </ul>
   </div>
 
   <div class="boucle-for">
     <h2>Boucle For</h2>
-    <p class="text-light">
-
-    </p>
+    <ul class='row col-4 list-group list-group-flush mx-auto mb-4 border bg-white text-center'>
+        <?php
+        $keys = array_keys($products);
+        for ($i=0; $i < count($products) ; $i++) {
+            echo "$keys[$i] : <br>";
+            $keys_value = array_keys($products[$keys[$i]]);
+            $value = array_values($products[$keys[$i]]);
+            for ($ii=0; $ii < count($keys_value) ; $ii++) {
+                if ($ii === 4) {
+                  echo "<li class='list-group-item'>";
+                  echo "$keys_value[$ii] : <img src='$value[$ii]' alt='image baskettes'><br>";
+                  echo "</li>";
+                } else {
+                  echo "<li class='list-group-item'>";
+                  echo "$keys_value[$ii] : $value[$ii]<br>";
+                  echo "</li>";
+            }
+            }
+            echo "<br>";
+        }
+         ?>
+    </ul>
   </div>
 
   <div class="boucle-foreach">
     <h2>Boucle Foreach</h2>
-    <p class="text-light">
+    <ul class='row col-4 list-group list-group-flush mx-auto mb-4 border bg-white text-center'>
+    <?php
+        $i = 0;
+        $keys = array_keys($products);
+        foreach($products as $products[$i]){
+            $ii = 0;
+            echo "$keys[$i] : <br>";
+            $keys_value = array_keys($products[$keys[$i]]);
+            $value = array_values($products[$keys[$i]]);
+            foreach ($keys_value as $keys_value[$ii]) {
+                if ($ii === 4) {
+                  echo "<li class='list-group-item'>";
+                  echo "$keys_value[$ii] : <img src='$value[$ii]' alt='image baskettes'><br>";
+                  echo "</li>";
+                  $ii = $ii +1;
+                } else {
+                  echo "<li class='list-group-item'>";
+                  echo "$keys_value[$ii] : $value[$ii]<br>";
+                  echo "</li>";
+                  $ii = $ii +1;
+            }
 
-    </p>
+        }
+        $i = $i+1;
+        }
+        ?>
+    </ul>
   </div>
 
   </body>
